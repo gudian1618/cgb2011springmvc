f4fe6f0e68977c2410832cc050c3e0f0902715a7
gudian1618 <1761844211@qq.com>
2021-03-15 19:48:18 +0800

    new

---
+++ src/main/java/com/github/gudian1618/cgb2011springmvc/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.github.gudian1618.cgb2011springmvc.controller;
+
+/**
+ * @author gudian1618
+ * @version v1.0
+ * @date 2021/3/15 7:31 下午
+ */
+
+public class HelloController {
+
+
+
+}
